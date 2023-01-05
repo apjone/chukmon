26e59539f0871644f608e377afd699c1b371ef6b
Tony Jones <tony@2nics.net>
2023-01-05 06:32:33 +0000

    added terms add/remove function

---
+++ app/chukmon_cron.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Borrowed form https://stackoverflow.com/a/49801719
+import time, traceback
+
+def every(delay, task):
+  next_time = time.time() + delay
+  while True:
+    time.sleep(max(0, next_time - time.time()))
+    try:
+      task()
+    except Exception:
+      traceback.print_exc()
+      # in production code you might want to have this instead of course:
+      # logger.exception("Problem while executing repetitive task.")
+    # skip tasks if we are behind schedule:
+    next_time += (time.time() - next_time) // delay * delay + delay
+
--- chukmon.py
+++ chukmon.py
@@ -10,10 +10,13 @@
 import sys, os, flask
 sys.path.append("app")
 from dotenv import load_dotenv
+from time import sleep
 from chukmon_version import *
 from chukmon_database import *
 from chukmon_companieshouse import *
 from chukmon_flask import *
+from chukmon_cron import *
+import threading
 
 # Load Enviroment file
 load_dotenv()
@@ -21,6 +24,7 @@ load_dotenv()
 DB_FILE = os.getenv('DB_FILE')
 APP_PORT = os.getenv('APP_PORT')
 APP_HOST = os.getenv('APP_HOST')
+POLL_TIME = int(os.getenv('POLL_TIME'))
 
 # Let's run some checks on the DB
 chuk_ver()
@@ -29,6 +33,7 @@ chuk_checkdb(DB_FILE)
 print()
 print("\033[4mPreflight DB checks passed, Lets go\033[0m")
 
+threading.Thread(target=lambda: every(POLL_TIME, chukmon_chinfo)).start()
 
 if __name__ == '__main__':
     app.run(debug=False, host=APP_HOST, port=APP_PORT)
